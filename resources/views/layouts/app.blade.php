abba88b0428ddfd1c5c27578ef3925679b7dcf2f
adiartawibawa <surat.buat.adi@gmail.com>
2022-01-13 08:06:48 +0800

    update styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/png" href="/images/logo.png"/>
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
